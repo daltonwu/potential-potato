1b5d78c588890a75ba728e268dd2cc516335afc6
TiffL <lei.tiffany@gmail.com>
2016-03-28 17:38:16 -0400

    edits

---
+++ app.js
@@ -0,0 +1,21 @@
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+//<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+
+d3.csv("UnemploymentRates.csv", function(data){
+    console.log(data[2]);
+});
+
+d3.csv("UnemploymentRates.csv", function(data){
+    data.forEach(function(d){
+	d["2009"] = +d["2009"];
+	d["2010"] = +d["2010"];
+	d["2011"] = +d["2011"];	
+	d["2012"] = +d["2012"];	
+	d["2013"] = +d["2013"];
+	d["2014"] = +d["2014"];
+	d["2015"] = +d["2015"];
+	d[""] = +d[""];
+    });
+    console.log(data[2]);
+});
+
